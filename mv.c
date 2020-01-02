ab3cd7af3ee418bbd5843b30b4ca473d650f96c7
AgostinoCS <agostino.dilillo.cs@gmail.com>
2020-01-02 11:16:58 +0100

    Add utils.h

--- mv.c
+++ mv.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "utils.h"
 
 void usage(char *);
-void fileCopy(FILE *, FILE *);
 
 int main(int argc, char *argv[]){
     if (argc < 3){
@@ -26,11 +26,4 @@ int main(int argc, char *argv[]){
 void usage(char *progName){
     puts("mv - move files");
     printf("Usage: %s SOURCE DESTINATION\n", progName);
-}
-
-void fileCopy(FILE *src, FILE *dest){
-    int c;
-    while((c=fgetc(src)) != EOF){
-        fputc(c, dest);
-    }
 }
