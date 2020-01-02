7ebd2afb52364645cf7df8eaf9a1e532a4dc980b
AgostinoCS <agostino.dilillo.cs@gmail.com>
2020-01-02 14:37:55 +0100

    Add cp.c

---
+++ cp.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+void usage(char *);
+void help(char *);
+
+int main(int argc, char *argv[]){
+    if (argc < 3){
+        fprintf(stderr, "%s: too few arguments\n", argv[0]);
+        usage(argv[0]);
+        exit(-1);
+    }
+    if(strcmp(argv[1], "--help") == 0){
+        help(argv[0]);
+        exit(1);
+    }
+    if (strcmp(argv[1], "--version") == 0){
+        puts(VERSION);
+        exit(2);
+    }
+    FILE *from = fopen(argv[1], "r");
+    FILE *to = fopen(argv[2], "w");
+    fileCopy(from, to);
+    return 0;
+}
+
+
+void usage(char *progName){
+    printf("\nUsage: %s [OPTIONS] SOURCE DESTINATION\n", progName\n);
+}
+
+void help(char *progName){
+    printf("\ncp - copy files\n")
+    usage(progName);
+    printf("\n--help\tdisplay this help and exit\n");
+    printf("--version\t output version information and exit\n\n");
+}
+
