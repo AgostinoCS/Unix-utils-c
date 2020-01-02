0211338bfa47ccd22fc429244456efb654a5df6f
AgostinoCS <agostino.dilillo.cs@gmail.com>
2020-01-02 19:12:49 +0100

    Bug fixes and add some controls

--- cp.c
+++ cp.c
@@ -7,7 +7,11 @@ void usage(char *);
 void help(char *);
 
 int main(int argc, char *argv[]){
-
+    if (argc < 2){
+        fprintf(stderr, "%s: too few arguments\n", argv[0]);
+        usage(argv[0]);
+        exit(-1);
+    } 
     if(strcmp(argv[1], "--help") == 0){
         help(argv[0]);
         exit(1);
@@ -20,9 +24,16 @@ int main(int argc, char *argv[]){
         fprintf(stderr, "%s: too few arguments\n", argv[0]);
         usage(argv[0]);
         exit(-1);
+    }
+    FILE *from, *to;
+    if ((from = fopen(argv[1], "r")) == NULL){
+        fprintf(stderr, "%s: cannot open %s\n", argv[0], argv[1]);
+        exit(-2);
+    }
+    if ((to = fopen(argv[2], "r")) == NULL){
+        fprintf(stderr, "%s: cannot open or create %s\n", argv[0], argv[2]);
+        exit(-3);
     }    
-    FILE *from = fopen(argv[1], "r");
-    FILE *to = fopen(argv[2], "w");
     fileCopy(from, to);
     return 0;
 }
@@ -38,4 +49,3 @@ void help(char *progName){
     printf("\n--help\tdisplay this help and exit\n");
     printf("--version\t output version information and exit\n\n");
 }
-
