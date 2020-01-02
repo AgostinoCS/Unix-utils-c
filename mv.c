6e889a305401a5a25b3aebcb56cc7fbfedd090a2
AgostinoCS <agostino.dilillo.cs@gmail.com>
2020-01-02 18:57:28 +0100

    Add --version and --help options

--- mv.c
+++ mv.c
@@ -1,19 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include "utils.h"
 
 void usage(char *);
+void help(char *);
 
 int main(int argc, char *argv[]){
+    if (argc < 2){
+        fprintf(stderr, "%s: too few arguments\n", argv[0]);
+        usage(argv[0]);
+        exit(-1);
+    }    
+    if(strcmp(argv[1], "--help") == 0){
+        help(argv[0]);
+        exit(1);
+    }
+    if (strcmp(argv[1], "--version") == 0){
+        puts(VERSION);
+        exit(2);
+    } 
     if (argc < 3){
         fprintf(stderr, "%s: too few arguments\n", argv[0]);
         usage(argv[0]);
-        exit(1);
-    }
+        exit(-1);
+    }       
     if (argc > 3){
         fprintf(stderr, "%s: too many arguments\n", argv[0]);
         usage(argv[0]);
-        exit(2);
+        exit(-2);
     }
     FILE *from = fopen(argv[1], "r");
     FILE *to = fopen(argv[2], "w");
@@ -24,6 +39,12 @@ int main(int argc, char *argv[]){
 
 
 void usage(char *progName){
-    puts("mv - move files");
-    printf("Usage: %s SOURCE DESTINATION\n", progName);
+    printf("\nUsage: %s SOURCE DESTINATION\n", progName);
+}
+
+void help(char *progName){
+    printf("\nmv - move files\n");
+    usage(progName);
+    printf("\n--help\tdisplay this help and exit\n");
+    printf("--version\t output version information and exit\n\n");    
 }
